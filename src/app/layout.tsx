f60276205caad1e5f976d123c9cd2ca87396227f
Isaac Hussain <isaaclearns58@gmail.com>
2025-01-30 05:45:56 +0300

    chore: set up firebase along with .env files also deploy to vercel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { FIREBASE_AUTH, FIREBASE_DB } from "../../firebase";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,13 +14,13 @@ export default function Home() {
         />
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
-            Get started by editing{" "}
+            firebase auth and firestore set up correctly
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
               src/app/page.tsx
             </code>
             .
           </li>
-          <li>Save and see your changes instantly.</li>
+          <li>checking live update on the hosted server</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
