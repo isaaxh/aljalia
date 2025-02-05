df31d79340601f83c36aaa1da95e0cc33e4c5253
Isaac Hussain <isaaclearns58@gmail.com>
2025-02-06 01:58:14 +0300

    feat: add necessacery imports to login page

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,12 +1,88 @@
+"use client";
+
+import React, { FormEvent, useEffect, useState, useTransition } from "react";
+import {
+  ConfirmationResult,
+  RecaptchaVerifier,
+  signInWithPhoneNumber,
+} from "firebase/auth";
+import {
+  InputOTP,
+  InputOTPGroup,
+  InputOTPSeparator,
+  InputOTPSlot,
+} from "@/components/ui/input-otp";
+import { REGEXP_ONLY_DIGITS } from "input-otp";
+import { useRouter } from "next/navigation";
 import PhoneNumberForm from "@/components/PhoneNumberForm";
-import React from "react";
+import { FIREBASE_AUTH } from "../../../../firebase";
 
 function LoginPage() {
-  console.log("testing from loginpage");
+  const router = useRouter();
+
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [otp, setOtp] = useState("");
+  const [error, setError] = useState<string | null>(null);
+  const [success, setSuccess] = useState("");
+  const [resendCountdown, setResendCountdown] = useState(0);
+
+  const [recaptchaVerifier, setRecaptchaVerifier] =
+    useState<RecaptchaVerifier | null>(null);
+
+  const [confirmationResult, setConfirmationResult] =
+    useState<ConfirmationResult | null>(null);
+
+  const [isPending, startTransition] = useTransition();
+
+  useEffect(() => {
+    let timer: NodeJS.Timeout;
+
+    if (resendCountdown > 0) {
+      timer = setTimeout(() => setResendCountdown(resendCountdown - 1), 1000);
+    }
+    return () => clearTimeout(timer);
+  }, [resendCountdown]);
+
+  useEffect(() => {
+    const recaptchaVerifier = new RecaptchaVerifier(
+      FIREBASE_AUTH,
+      "recaptcha-container",
+      {
+        size: "invisible",
+      },
+    );
+
+    setRecaptchaVerifier(recaptchaVerifier);
+
+    return () => {
+      recaptchaVerifier.clear();
+    };
+  }, [FIREBASE_AUTH]);
+
   return (
-    <div className="max-w-[1600px] flex flex-col flex-1 items-center justify-center">
-      <PhoneNumberForm />
-    </div>
+    <>
+      <h1>Testing otp input</h1>
+
+      {!confirmationResult && <PhoneNumberForm />}
+
+      {/* <InputOTP maxLength={6} pattern={REGEXP_ONLY_DIGITS}> */}
+      {/*   <InputOTPGroup> */}
+      {/*     <InputOTPSlot index={0} /> */}
+      {/*     <InputOTPSlot index={1} /> */}
+      {/*     <InputOTPSlot index={2} /> */}
+      {/*   </InputOTPGroup> */}
+      {/*   <InputOTPSeparator /> */}
+      {/*   <InputOTPGroup> */}
+      {/*     <InputOTPSlot index={3} /> */}
+      {/*     <InputOTPSlot index={4} /> */}
+      {/*     <InputOTPSlot index={5} /> */}
+      {/*   </InputOTPGroup> */}
+      {/* </InputOTP> */}
+      {/**/}
+      {/* <Button>Submit</Button> */}
+
+      <div id="recaptcha-container" />
+    </>
   );
 }
 
---
+++ src/app/(auth)/otp/page.tsx
@@ -0,0 +1,12 @@
+import OtpLogin from "@/components/OtpLogin";
+import React from "react";
+
+function OTPConfirmationPage() {
+  return (
+    <>
+      <OtpLogin />
+    </>
+  );
+}
+
+export default OTPConfirmationPage;
