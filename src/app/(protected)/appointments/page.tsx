7dfa344c96728bafeabaae0fcbf1157b58358d2e
Isaac Hussain <isaaclearns58@gmail.com>
2025-04-08 06:33:56 +0300

    feat: logout if user is not logged in and redirect to login page

--- src/app/(protected)/appointments/page.tsx
+++ src/app/(protected)/appointments/page.tsx
@@ -1,6 +1,16 @@
+"use client";
+
+import { useAuth } from "@/hooks/useAuth";
+import { useCheckAuth } from "@/hooks/useCheckAuth";
 import React from "react";
 
 function Appointments() {
+  const { logOut } = useAuth();
+  const { decodedUser, loading } = useCheckAuth();
+
+  if (!loading && !decodedUser) {
+    logOut();
+  }
   return <div>Appointments</div>;
 }
 
