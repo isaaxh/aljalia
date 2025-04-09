17d46cc097f3e24b3082a3830eb787784dce9be5
Isaac Hussain <isaaclearns58@gmail.com>
2025-04-09 09:06:37 +0300

    refactor: place useCheckAuth inside the useEffect

--- src/hooks/useCheckAuth.ts
+++ src/hooks/useCheckAuth.ts
@@ -7,35 +7,35 @@ export const useCheckAuth = () => {
   const { decodedUser, setDecodedUser } = useAuth();
   const [loading, setLoading] = useState(true);
 
-  const checkAuth = async () => {
-    try {
-      setLoading(true);
-      const res = await fetch("/api/auth", {
-        headers: {
-          "x-auth-token":
-            document.cookie
-              .split("; ")
-              .find((row) => row.startsWith("authToken="))
-              ?.split("=")[1] || "",
-        },
-      });
-      const data: DecodedIdToken = await res.json();
-
-      if (data.success) {
-        console.log("Authenticated User:", data.user);
-        setDecodedUser(data.user);
-        setLoading(false);
-      } else {
-        console.error("Auth failed:", data.error);
-        setLoading(false);
-      }
-    } catch (error) {
-      console.error("Auth request error:", error);
-      setLoading(false);
-    }
-  };
 
   useEffect(() => {
+    const checkAuth = async () => {
+      try {
+        setLoading(true);
+        const res = await fetch("/api/auth", {
+          headers: {
+            "x-auth-token":
+              document.cookie
+                .split("; ")
+                .find((row) => row.startsWith("authToken="))
+                ?.split("=")[1] || "",
+          },
+        });
+        const data: DecodedIdToken = await res.json();
+
+        if (data.success) {
+          setDecodedUser(data.user);
+          setLoading(false);
+        } else {
+          console.error("Auth failed:", data.error);
+          setLoading(false);
+        }
+      } catch (error) {
+        console.error("Auth request error:", error);
+        setLoading(false);
+      }
+    };
+
     checkAuth();
   }, []);
 
